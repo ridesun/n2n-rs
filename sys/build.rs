03efb28dd12002bc0242059b0635ad0d90a7210d
ridesun <1400963424@qq.com>
2024-06-15 20:59:46 +0800

    change build.rs

--- sys/build.rs
+++ sys/build.rs
@@ -1,7 +1,53 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
+    if !Path::new("./n2n/libn2n.a").exists() {
+        if !Path::new("./n2n").exists() {
+            if !std::process::Command::new("git")
+                .arg("clone")
+                .arg("https://github.com/ntop/n2n.git")
+                .output()
+                .expect("")
+                .status
+                .success()
+            {
+                panic!("git error")
+            }
+        }
+        let cur_path = PathBuf::from("./n2n/")
+            .canonicalize()
+            .expect("cannot canonicalize path");
+        if !std::process::Command::new("bash")
+            .current_dir(cur_path.clone())
+            .arg("./autogen.sh")
+            .output()
+            .expect("")
+            .status
+            .success()
+        {
+            panic!("autogen error")
+        }
+        if !std::process::Command::new("bash")
+            .current_dir(cur_path.clone())
+            .arg("./configure")
+            .output()
+            .expect("")
+            .status
+            .success()
+        {
+            panic!("configure error")
+        }
+        if !std::process::Command::new("make")
+            .current_dir(cur_path.clone())
+            .status()
+            .expect("")
+            .success()
+        {
+            panic!("make error")
+        }
+    }
+
     let libdir_path = PathBuf::from("./n2n/")
         .canonicalize()
         .expect("cannot canonicalize path");
@@ -12,44 +58,6 @@ fn main() {
 
     println!("cargo:rustc-link-lib=n2n");
 
-    // if !std::process::Command::new("git")
-    //     .arg("clone")
-    //     .arg("https://github.com/ntop/n2n.git")
-    //     .output()
-    //     .expect("")
-    //     .status
-    //     .success()
-    // {
-    //     panic!("autogen error")
-    // }
-    // if !std::process::Command::new("bash")
-    //     .current_dir(libdir_path.clone())
-    //     .arg("./autogen.sh")
-    //     .output()
-    //     .expect("")
-    //     .status
-    //     .success()
-    // {
-    //     panic!("autogen error")
-    // }
-    // if !std::process::Command::new("bash")
-    //     .current_dir(libdir_path.clone())
-    //     .arg("./configure")
-    //     .output()
-    //     .expect("")
-    //     .status
-    //     .success()
-    // {
-    //     panic!("configure error")
-    // }
-    // if !std::process::Command::new("make")
-    //     .current_dir(libdir_path.clone())
-    //     .status()
-    //     .expect("")
-    //     .success()
-    // {
-    //     panic!("make error")
-    // }
     let bindings = bindgen::Builder::default()
         .clang_args(["-I", libdir_path.clone().join("include").to_str().unwrap()])
         .ctypes_prefix("libc")
